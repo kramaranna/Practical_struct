1ab5f9f53131c4d8b1cd4a8513ed8643f6b68bee
kramaranna <kramaranna123@gmail.com>
2025-10-16 10:48:40 +0300

    Код змінено
    Всі зміни внесено

--- cpp_examples/student_example.cpp
+++ cpp_examples/student_example.cpp
@@ -5,14 +5,16 @@ using namespace std;
 struct Student {
     string name;
     int age;
-    float grade; // 👈 додайте  ще 1-2 поля, наприклад email або course
+    float grade;
+    string email;
+    int course;
 };
 
 int main() {
-    Student s1 = {"", 0, 0.0}; // 👈 заповнити дані
-    Student s2;                 // 👈  ініціалізувати другий об'єкт
-
-    cout << s1.name << " " << s1.age << " " << s1.grade << endl;
-
+    Student s1 = {"Vasya", 17, 4.4, "vasya@gmail.com", 1}; 
+    Student s2 = {"Inna", 19, 3.9, "inna@gmail.com", 3};            
+    
+    cout << s1.name << "  " << s1.age << "  " << s1.grade<< "  "<< s1.email << "  " << s1.course << "  "  << endl;
+    cout << s2.name << "  " << s2.age << "  " << s2.grade<< "  "<< s2.email << "  " << s2.course << "  "  << endl;
     return 0;
 }
